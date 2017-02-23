b1f98738c1dc9dbe8ed38789200dc429f10eef75
dhackelman <drew.hackelman@gmail.com>
2017-02-23 16:50:53 -0500

    include state setup for songlist and friend list

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,26 @@
-console.log('yo!');
-console.log('no!');
+(function () {
+
+  angular.module('MusicStreaming', ['ui.router', 'LocalStorageModule']);
+
+  angular.module('MusicStreaming').config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    console.log('in');
+
+    $stateProvider.state('MusicParent', {
+      url:'/',
+      abstract: true,
+      template: '<ui-view></ui-view>'
+    }).state('MusicParent.index', {
+        url: '',
+        templateUrl: './templates/home.html'
+    }).state('MusicParent.songlist', {
+        url: 'song-list',
+        templateUrl: './templates/song-list.html'
+        // controller: 'ListController as listCtrl'
+    }).state('MusicParent.friendlist', {
+      url: 'friend-list',
+      templateUrl: './templates/friend-list.html'
+    });
+  });
+
+})();
